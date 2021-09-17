3ad5468e9937943c5ff67d51eb9a328856169f09
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-16 20:43:02 -0400

    req.params practice

--- firstServer.js
+++ firstServer.js
@@ -10,15 +10,32 @@ app.get("/dogs", (req, res) => {
     res.send("<h1>I am dog woof woof</h1>");
 });
 
-//app.listen should be at the end of the file
-app.listen(3000, function () {
-    console.log("App on port 3000");
-});
-
 app.get("/chickens", (req, res) => {
     res.send("Bock!! bock bock(get request)");
 });
 
 app.post("/chickens", function createchicken(req, res) {
     res.send("You created a new chicken (not really) (post request)");
+});
+const greetings = {
+    en: "hello",
+    fr: "bonjour",
+    ic: "hallo",
+    ja: "konnichiwa",
+};
+
+app.get("/greet/:language/:color", (req, res) => {
+    console.log(req.params);
+    const lang = req.params.language;
+    const color = req.params.color;
+    const greeting = greetings[lang];
+    console.log(lang, color);
+    if (!greeting) return res.send("INVALID LANGUAGE");
+
+    return res.send(greeting.toUpperCase());
+});
+
+//app.listen should be at the end of the file
+app.listen(3000, function () {
+    console.log("App on port 3000");
 });
